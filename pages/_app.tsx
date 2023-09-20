03fd542fd5055a4944063af3f86a14f52ece9c8e
unknown <canon193@gmail.com>
2023-09-20 20:11:55 +0700

    Add google analytic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,14 @@ import type { AppProps } from 'next/app'
 import 'aos/dist/aos.css'
 import AOS from 'aos'
 import { useEffect } from 'react'
+import TagManager from 'react-gtm-module';
+
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     AOS.init({
       duration: 600
     })
+    TagManager.initialize({ gtmId: process.env.GOOGLE_ANALYTIC_WEBSITE_ID });
   }, [])
   return <Component {...pageProps} />
 }
