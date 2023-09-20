20299d926741d3c3c1bfc720470b9b520b0e2253
kha.nguyen <kha.nguyen@isobar.com>
2023-09-20 22:28:53 +0700

    Edit content in quote page

--- pages/quote.tsx
+++ pages/quote.tsx
@@ -1,53 +1,61 @@
 import Button from 'components/atoms/Button'
-import ButtonLink from 'components/atoms/Button/ButtonLink'
 import InputGroup from 'components/molecules/FormGroup/InputGroup'
-import SelectGroup from 'components/molecules/FormGroup/SelectGroup'
 import TextAreaGroup from 'components/molecules/FormGroup/TextAreaGroup'
+import IconListItem from 'components/molecules/IconListItem'
 import PageSentence from 'components/molecules/PageSentence'
 import PageTemplate from 'components/templates/PageTemplate'
 import React from 'react'
+import { FiMail, FiPhoneCall, FiSlack } from 'react-icons/fi'
+import {LiaSkype} from 'react-icons/lia'
 
 const Quote = () => {
   return (
     <>
-      <PageTemplate title='Send Quote - Your Tech Partner'>
-        <section className="grid grid-cols-1 place-items-center gap-5 lg:grid-cols-2">
-          <aside className="w-full sm:w-10/12 md:w-8/12 grid grid-cols-1 place-items-center gap-12 lg:w-full lg:place-items-start" data-aos="fade-down-right">
-            <div className="text-center lg:text-left">
+      <PageTemplate title='Contact - Your Tech Partner'>
+        <section className="grid grid-cols-1 place-items-center gap-10 lg:gap-5 lg:grid-cols-2">
+          <aside className="w-full sm:w-10/12 md:w-8/12 grid grid-cols-1 gap-12 sm:place-items-center lg:w-full lg:place-items-start" data-aos="fade-up-right">
+            <div className="sm:text-center lg:text-left">
               <PageSentence
-                title="Tell us about your problem and how we can help"
-                description="We are happy to help you, tell us what is the problem with your company, and we are ready to answer these problems. It usually takes a few minutes for us to respond."
-                badge="SEND QUOTE"
+                badge="CONTACT"
+                title="We love receiving messages from you, we are waiting for it."
               />
             </div>
-            <div className="w-fit">
-              <ButtonLink
-                value="Ask Us"
-                href="/faq"
-                color="white"
-                style="light"
-              />
+            <div className="space-y-6">
+              <div className="grid grid-cols-2 gap-4">
+                <IconListItem
+                  label="Slack"
+                  value=""
+                  icon={<FiSlack />}
+                />
+                <IconListItem
+                  label="Skype"
+                  value=""
+                  icon={<LiaSkype />}
+                />
+              </div>
+              <div className="grid grid-cols-2 gap-4">
+                <IconListItem
+                  label="Phone"
+                  value="+62 1234 8921"
+                  icon={<FiPhoneCall />}
+                />
+                <IconListItem
+                  label="Email"
+                  value="support@your-tech-partner.com"
+                  icon={<FiMail />}
+                />
+              </div>
             </div>
           </aside>
-          <aside className="w-full sm:w-10/12 md:w-8/12 lg:w-full lg:flex lg:justify-end" data-aos="fade-up-left">
+          <aside className="w-full sm:w-10/12 md:w-8/12 lg:w-full lg:flex lg:justify-end" data-aos="fade-down-left">
             <div className="grid grid-cols-1 gap-7 p-6 md:p-9 bg-light rounded-md lg:w-10/12 ">
               <div className="grid grid-cols-2 gap-4">
                 <InputGroup label="Name" />
                 <InputGroup label="Email" />
               </div>
-              <div className="grid grid-cols-2 gap-4">
-                <InputGroup label="Company" />
-                <SelectGroup
-                  label="Company Size"
-                  options={[
-                    { label: 'Small', value: 'small' },
-                    { label: 'Medium', value: 'medium' },
-                    { label: 'Large', value: 'large' },
-                  ]}
-                />
-              </div>
-              <TextAreaGroup label="Tell Us Your Problem" />
-              <Button value="Send Quote" />
+              <InputGroup label="Subject" />
+              <TextAreaGroup label="Message" />
+              <Button value="Send Message" />
             </div>
           </aside>
         </section>
